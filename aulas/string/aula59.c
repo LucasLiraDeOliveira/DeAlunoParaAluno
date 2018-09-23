183eb87ee3f4a4d96dc8e06985e8481ecd557ad5
Lucas Lira De Oliveira <lucas.oliveira845@gmail.com>
2018-09-23 09:08:29 -0300

    adicionado aula 59

--- aulas/string/aula59.c
+++ aulas/string/aula59.c
@@ -28,31 +28,72 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void alfabetico(char variavel);
+#define MAX 20
+#define NUMERODEDEFINICOES 7
+
+struct dicionario
+{
+    char palavra[20];
+    char definicao[50];
+};
+
+bool compararStrings(const char palavra1[], const char palavra2[]);
+int procurarStrings(const struct dicionario lingua[], const char procurar[], const int numDePalavras);
 
 int main(void)
 {
-    char nome[20];
-    int i = 0;
+    char palavra[MAX] = {'\0'};
+    int resultadoPesquisa; 
 
-    printf("Digite uma palavra:\n");
-    scanf("%s", &nome);
-    
-    while(nome[i] != '\0')
-    {
-        alfabetico(nome[i]);
-        i++;    
-    } 
+    const struct dicionario portugues[NUMERODEDEFINICOES] = {{"pao", "Comida de farinha"}, 
+                                                             {"mortadela", "Comida de carne"}, 
+                                                             {"feijao", "comida brasileira"}, 
+                                                             {"tropeiro", "Tipo de feijao"}, 
+                                                             {"queijo", "De minas"}, 
+                                                             {"macarronada", "Tipico de vo'"}, 
+                                                             {"pizza", "Tipico da Italia"}};
+
+    printf("Digite a uma palavra: ");
+    scanf("%s", palavra);
+
+    resultadoPesquisa = procurarStrings(portugues, palavra, NUMERODEDEFINICOES);
+    if(resultadoPesquisa  != -1 )
+        printf("%s\n", portugues[resultadoPesquisa].definicao);
+    else
+        printf("Palavra nao encontrada\n");
+
     return EXIT_SUCCESS;
 }
 
-void alfabetico(char variavel)
+bool compararStrings(const char palavra1[], const char palavra2[])
 {
-    if((variavel >= 'a' && variavel <= 'z') || (variavel >= 'A' && variavel <= 'Z'))
-        printf("%c e' uma letra\n", variavel);
+    int i = 0;
+
+    while(palavra1[i] == palavra2[i] && palavra1[i] != '\0' && palavra2[i] != '\0')
+        i++;
+
+    if(palavra1[i] == '\0' && palavra2[i] == '\0')
+        return true;
     else
-        printf("%c nao e' uma letra\a\n", variavel);
-    return 0;
+        return false;
 }
+
+int procurarStrings(const struct dicionario lingua[], const char procurar[], const int numDePalavras)
+{
+    int i = 0;
+
+    while(i < numDePalavras) 
+    {   
+        if(compararStrings(procurar, lingua[i].palavra))
+            return i;
+        else
+            i++;
+    }
+
+    return -1;
+}
+
+
 
